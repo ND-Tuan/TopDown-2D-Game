b0973b7cc2f855d6278d361642e7b40f48c108dc
Tunglam4902 <130317110+Tunglam4902@users.noreply.github.com>
2024-01-04 11:16:05 +0700

    Update CheckEnding.cs

--- Assets/Scripts/UIController/CheckEnding.cs
+++ Assets/Scripts/UIController/CheckEnding.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class CheckEnding : MonoBehaviour
 {
+    // Tham chiếu đến đối tượng CallMenu
     private CallMenu callMenu;
+
     void Start()
     {
+        // Lấy tham chiếu đến đối tượng CallMenu từ tag "Menu"
         callMenu = GameObject.FindGameObjectWithTag("Menu").GetComponent<CallMenu>();
     }
 
-     void OnTriggerEnter2D(Collider2D other){
-        if(other.CompareTag("Player")){
+    // Phương thức được gọi khi Collider2D kích hoạt va chạm
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Kiểm tra xem đối tượng va chạm có tag là "Player" không
+        if (other.CompareTag("Player"))
+        {
+            // Gọi phương thức DisplayChange() từ đối tượng callMenu
             callMenu.DisplayChange();
         }
     }
 
-    public void  DisplayResult(){
+    // Phương thức hiển thị kết quả
+    public void DisplayResult()
+    {
+        // Gọi phương thức DisplayResult() từ đối tượng callMenu
         callMenu.DisplayResult();
     }
-
 }
